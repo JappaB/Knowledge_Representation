9dc2700e67bb365bfea2588e6d4f3ddcc5d95d12
aronhammond <hammond756@live.nl>
2017-10-17 11:10:48 +0200

    added function to generate all states

--- QR assignment/sink_main.py
+++ QR assignment/sink_main.py
@@ -21,8 +21,22 @@ def quantity_addition(q1, q2):
     if q1 != q2:
         return AMB
 
-def generate_state(state1):
-    raise NotImplementedError()
+def generate_all_states():
+    derivative_values = [NEG, STD, POS]
+    states = []
+
+    for derivative_in in derivative_values:
+        for derivative_out in  derivative_values:
+            for derivative_vol in derivative_values:
+                for magnitude_in in [ZER, POS]:
+                    for magnitude_out in [ZER, POS, MAX]:
+                        for magnitude_vol in [ZER, POS, MAX]:
+                            volume = Volume(magnitude_vol, derivative_vol)
+                            inflow = Inflow(magnitude_in, derivative_in)
+                            outflow = Outflow(magnitude_out, derivative_out)
+                            states.append(State(inflow, outflow, volume))
+
+    return states
 
 
 # state1: starting state
@@ -32,13 +46,18 @@ def valid_transition(state1, state2):
 
     raise NotImplementedError()
 
-    # Continuity (derivatives can't change sign without passing STD
+    # Continuity (derivatives can't change sign without passing STD)
+    # Also holds for quantities (can't go from ZER to MAX)
+
+    # Can't be max with positive derivative or zero with negative
 
     # Derivative has to change before magnitude
 
     # Point quantities change before ranges
 
     # All quantity calculus checks out
+
+    # Value Control relations must hold
 
 conceptual_model = {
     "outflow" : [("volume", "I", NEG)],
@@ -110,6 +129,19 @@ class State:
     outflow = None
     inflow = None
 
+    conceptual_model = {
+        outflow : [(volume, "I", NEG)],
+        volume : [(outflow, "P", POS), ("outflow", "VC", MAX), ("outflow", "VC", ZER)],
+        inflow : [(volume, "I", POS)]
+    }
+
+    # def __str__(self):
+    #     pretty_print = "Quantity | Magnitude | Derivative" \
+    #                    "Volume:  | " + self.volume.magnitude + " | " + self.volume.derivative + "\n" \
+    #                    "" \
+    #                    ""
+    #     return
+
     def __init__(self, inf, outf, vol):
         self.volume = vol
         self.outflow = outf
@@ -124,6 +156,7 @@ class State:
     def __ne__(self, other):
         return not self.__eq__(other)
 
+
 def main():
     vol = Volume(NEG, NEG)
     outf = Outflow(NEG, NEG)
@@ -131,11 +164,11 @@ def main():
 
 
     # Test influence addition
-    for q1 in [NEG, STD, POS]:
-        for q2 in [NEG, STD, POS]:
-            print(q1 + " and ", q2, '=', infl_addition(q1, q2))
+    # for q1 in [NEG, STD, POS]:
+    #     for q2 in [NEG, STD, POS]:
+    #         print(q1 + " and ", q2, '=', quantity_addition(q1, q2))
 
-
+    states = generate_all_states()
 
 if __name__ == '__main__':
 	main()
