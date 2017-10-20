693f561cd19f4efc044c482ab12d1a8a46328407
JappaB <jcsbakker@gmail.com>
2017-10-20 16:28:34 +0200

    continuity boolean

--- QR assignment/sink_main.py
+++ QR assignment/sink_main.py
@@ -69,6 +69,8 @@ def index_distance(l, item1, item2):
     i1 = l.index(item1)
     i2 = l.index(item2)
 
+    # print "position in",l,"state1",i1,"state2",i2
+    print abs(i1 - i2)
     return abs(i1 - i2)
 
 def valid_state(state):
@@ -138,52 +140,46 @@ def valid_transition(state1, state2):
     state1_values = state1.get_state()
     state2_values = state2.get_state()
 
-    instable_quants = instable_transition_state(state1)
-    if len(instable_quants):
-        print "instable quantities:", instable_quants, state1.id, state2.id
+    # if instable state => go to stable state
 
-    for label, quantity in state1_values.iteritems():
+    # Next state should be a continuous child of previous state
+    continuous = True
+    # Check if it really is in line with the rule
+    if continuous:
+        for label, quantity in state1_values.iteritems():
+            print state1_values[label]
 
-        # Continuity (derivatives can't change sign without passing STD)
-        # Also holds for quantities (can't go from ZER to MAX)
-        # Check if derivative changes with more than a single step
-        derivative_change = index_distance(quantity.der_q_space, quantity.derivative, state2_values[label].derivative)
+            # Continuity (derivatives can't change sign without passing STD)
+            # Also holds for quantities (can't go from ZER to MAX)
+            # Check if derivative changes with more than a single step
+            derivative_change = index_distance(quantity.der_q_space, quantity.derivative, state2_values[label].derivative)
 
+            if derivative_change > 1:
+                continuous = False
 
+            # Check if magnitude changes with more than a single step
+            magnitude_change = index_distance(quantity.mag_q_space, quantity.magnitude, state2_values[label].magnitude)
+            if magnitude_change > 1:
+                continuous = False
 
-        if derivative_change != 1 and derivative_change != 0: return False
-
-        # Check if magnitude changes with more than a single step
-        magnitude_change = index_distance(quantity.mag_q_space, quantity.magnitude, state2_values[label].magnitude)
-
-        if magnitude_change != 1 and magnitude_change != 0: return False
 
         # Derivative has to change before magnitude
-        if derivative_change == 1 and magnitude_change != 0: return False
+
 
         # Magnitude can't change while derivative is zero
-        if quantity.derivative == ZER and magnitude_change != 0: return False
+
 
         # Point quantities change before ranges
         # mag: ZER, der: POS must transit to POS,POS
         # if (ZER+POS -> POS+POS) should hold
-        if not(not(quantity.symbol_pair() == ZER+POS) or state2_values[label].symbol_pair() == POS+POS): return False
+
 
         # mag: MAX, der: NEG must transit to POS,NEG
         # if (MAX+NEG -> POS+NEG) should hold
-        if not(not(quantity.symbol_pair()) == MAX+NEG or state2_values[label].symbol_pair() == POS+NEG): return False
+
 
         # You cannot change the inflow (derivative) during an instable point state,
         # eg. MAX+NEG or ZER+POS
-        if len(instable_quants) > 0:
-            # quantity is instable and does not change
-            if label in instable_quants and state1_values[label] == state2_values[label]:
-                print label, ":", state1.id, "->", state2.id
-                return False
-            # quantity not causing the instability changes
-            if label not in instable_quants and state2_values[label] != state2_values[label]:
-                return False
-
     return True
 
 class Inflow:
@@ -310,12 +306,17 @@ def main():
     #     state.set_id(i + 1)
 
     valid_transitions = []
+    counter =0
     for state1, state2 in itertools.combinations(valid_states, 2):
 
         if valid_transition(state1, state2):
             valid_transitions.append( (state1, state2) )
         if valid_transition(state2, state1):
             valid_transitions.append( (state2, state1) )
+
+        counter+=1
+        if counter == 20:
+            break
 
 
     create_state_graph(valid_states, valid_transitions)
